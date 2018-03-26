a8e87e2c304a1cd2e604c135cfd49dfdc58057e1
Tanmay Halde <tanmayhalde@gmail.com>
2018-03-26 01:14:19 -0400

    FIX: Removed redundant files and minor formating

--- models/Stock.js
+++
@@ -1,9 +0,0 @@
-let mongoose = require('mongoose');
-let Schema = mongoose.Schema;
-
-let stockSchema = new Schema ({
-  code: String
-});
-
-let Stock = mongoose.model('Stock', stockSchema);
-module.exports = Stock;
--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -25,7 +25,6 @@ class Chart extends Component {
           config: newConfig
         });
       })
- 
   }
 
   render() {
